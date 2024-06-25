3ba21b774c34679d11cffbb2c53b672b9e4060cf
Royston <roystonvishal1212@gmail.com>
2024-06-25 09:06:30 +0530

    Data Models are created

---
+++ RickyAndMorty/Models/RMOrigin.swift
@@ -0,0 +1,15 @@
+//
+//  RMOrigin.swift
+//  RickyAndMorty
+//
+//  Created by Royston Vishal on 30/05/24.
+//
+
+import Foundation
+
+struct RMOrigin: Codable {
+    
+    let name: String
+    let url: String
+    
+}
---
+++ RickyAndMorty/Models/RMSingleLocation.swift
@@ -0,0 +1,15 @@
+//
+//  RMSingleLocation.swift
+//  RickyAndMorty
+//
+//  Created by Royston Vishal on 30/05/24.
+//
+
+import Foundation
+
+struct RMSingleLocation: Codable {
+    
+    let name: String
+    let url: String
+    
+}
